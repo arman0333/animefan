654ce23b70b32d4ec7877dfaab799b3df291bfd1
armanx0333 <armansethi64@gmail.com>
2024-06-03 00:27:30 +0530

    anime transformation

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(
+<<<<<<< HEAD
     name="perceptive",
     version="1.1",
     description="Perceptive of life through the terminal",
@@ -9,4 +10,14 @@ setup(
     packages=["libperceptive"],
     scripts=["perceptive"], 
     package_data={"libperceptive": ["content/*"]},  
+=======
+    name="anime",
+    version="1.1",
+    description="Glimpse of Anime through the terminal",
+    long_description=open("README.md").read(),
+    license="MIT",
+    packages=["libanime"],
+    scripts=["anime"], 
+    package_data={"libanime": ["content/*"]},  
+>>>>>>> f3ff983 (anime transformation)
 )
