c8ca7c10b344cedb658c0a25f7cec2127807e6d6
arman0333 <129063079+arman0333@users.noreply.github.com>
2024-06-29 21:29:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="anime",
-    version="1.1",
-    description="Glimpse of Anime through the terminal",
-    long_description=open("README.md").read(),
-    # long_description_content_type="text/markdown",
-    author="Arman Sethi",  
-    author_email="armansethi64@gmail.com",  
-    url="https://github.com/arman0333/anime-cli", 
-    license="MIT",
-    packages=find_packages(),  
+    name='anime-cli',
+    version='1.0.0',
+    description='A CLI tool for anime quotes and ASCII art',
+    author='Arman Sethi',
+    author_email='armansethi64@gmail.com',
+    url='https://github.com/yourusername/anime-cli',
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
-        'click',  
+        'requests',    
+        'pyfiglet',
     ],
     entry_points={
         'console_scripts': [
-            'anime=libanime.cli:main', 
+        'anime-cli=libanime:_init_.py'
         ],
     },
-    package_data={
-        "libanime": ["content/*"],
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
 )
